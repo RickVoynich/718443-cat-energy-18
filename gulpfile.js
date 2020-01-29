335e2476d0fe43921a2882499cfa77176218fad6
Rick <46853857+RickVoynich@users.noreply.github.com>
2020-01-29 12:13:26 +0300

    Б8
    смена порядка шрифтов

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task("images", function () {
 
 gulp.task("copy", function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
+    "source/fonts/**/*.{woff2,woff}",
     "source/img/**",
     "source/js/**"
   ], {
